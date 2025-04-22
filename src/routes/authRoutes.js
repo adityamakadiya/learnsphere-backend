b5e45bebb2a1f0a314b3899f576da307372eeb87
adityamakadiya <adityamakadiya01@gmail.com>
2025-04-22 16:30:01 +0530

    [FEAT]: Register and Login

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const { register, login } = require('../controllers/authController');
+const { register, login, getMe } = require('../controllers/authController');
 const { validateRegister, validateLogin } = require('../middleware/validator');
+const auth = require('../middleware/auth');
 const router = express.Router();
 
 router.post('/register', validateRegister, register);
